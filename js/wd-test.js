be5e3662b415296e509d35ab3f44fc5e973ba706
Drini Cami <cdrini@gmail.com>
2015-06-03 00:28:27 -0400

    check more image properties for images

--- js/wd-test.js
+++ js/wd-test.js
@@ -35,10 +35,35 @@ function findLabel(entity) {
 	var label = entity.getLabel(lang);
 }
 function findImage(entity, smi) {
-	var imgs = entity.getClaim('P18') || // image
-	           entity.getClaim('P41') || // flag
-	           entity.getClaim('P948') || // wikivoyage banner
-	           entity.getClaim('P94'); // coat of arms
+	var imgs = entity.getFirstClaim(
+		'P18',      // image
+
+		'P154',     // logo image
+
+		'P41',      // flag image
+		'P948',     // Wikivoyage banner
+		'P692',     // Gene Atlas Image
+		'P1766',    // place name sign
+		'P94',      // coat of arms image
+		'P242',     // locator map image
+		'P15',      // road map
+		'P1621',    // detail map
+		'P1846',    // distribution map
+
+		'P14',      // highway marker
+
+		'P1801',    // commemorative plaque image
+		'P1543',    // monogram
+		'P158',     // seal image
+		'P1442',    // image of grave
+		'P109',     // signature
+
+		'P367',     // astronomic symbol image
+		'P491',     // orbit diagram
+		'P117',     // chemical structure
+		'P207',     // bathymetry image
+		'P181'      // taxon range map image
+	);          
 
 	if(!imgs) return null;
 
--- js/wikidata.js
+++ js/wikidata.js
@@ -2,9 +2,32 @@ var WD = {};
 WD.Entity = function(entity) {
 	this.entity = entity;
 }
+/**
+ * Gets the array of property's values. If not found, returns undefined.
+ *
+ * @param {String} prop - a property PID
+ * @return {Array} of values or undefined
+ */
 WD.Entity.prototype.getClaim = function(prop) {
 	return this.entity.claims[prop];
 }
+
+/**
+ * Returns the first of the arguments that has 1 or more claims. If none
+ * found, returns undefined.
+ *
+ * @param list of string property IDs
+ * @return {Array} of values or undefined
+ */
+WD.Entity.prototype.getFirstClaim = function() {
+	for (var i = 0; i < arguments.length; i++) {
+		if(this.getClaim(arguments[i])) {
+			return this.getClaim(arguments[i]);
+		}
+	};
+	return undefined;
+}
+
 WD.Entity.prototype.getLabel = function(langs, returnObject) {
 	if(langs instanceof String) {
 		langs = [ langs ];
