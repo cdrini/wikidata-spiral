85cdb093300c16b0196daa754f2ba9119c094ee2
Drini Cami <cdrini@gmail.com>
2019-01-31 21:08:25 -0500

    refactor: lite cleanup of wikidata.js + more docs

--- js/wikidata.js
+++ js/wikidata.js
@@ -1,22 +1,32 @@
+/**
+ * WD contains useful Wikidata-related functions and types.
+ */
+
 var WD = {};
+
+/**
+ * @constructor
+ * @param {Object} entity JSON of a WD entity
+ */
 WD.Entity = function(entity) {
   this.entity = entity;
 }
+
 /**
  * Gets the array of property's values. If not found, returns undefined.
- *
- * @param {String} prop - a property PID
- * @return {Array} of values or undefined
+ * @private
+ * @param {String} prop a property PID (like 'P279')
+ * @return {Array<object>|undefined} Array of JSON claim values
  */
-WD.Entity.prototype.getClaim = function(prop) {
+WD.Entity.prototype._getClaim = function(prop) {
   return this.entity.claims[prop];
 }
 
 /**
  * Get's a claim's value. Uses the first statement.
  *
- * @param {String} prop - a property PID
- * @return {String} The value
+ * @param {String} prop a property PID
+ * @return {String}
  */
 WD.Entity.prototype.getClaimValue = function(prop) {
   var claim = this.entity.claims[prop];
@@ -40,47 +50,50 @@ WD.Entity.prototype.getClaimValue = function(prop) {
  * Returns the first of the arguments that has 1 or more claims. If none
  * found, returns undefined.
  *
- * @param list of string property IDs
- * @return {Array} of values or undefined
+ * @param {Array<String>} list of string property IDs (like ["P279"])
+ * @return {Array<object>|undefined} JSON claims if any
  */
 WD.Entity.prototype.getFirstClaim = function() {
   for (var i = 0; i < arguments.length; i++) {
-    if(this.getClaim(arguments[i])) {
-      return this.getClaim(arguments[i]);
+    if(this._getClaim(arguments[i])) {
+      return this._getClaim(arguments[i]);
     }
   };
   return undefined;
 }
 
-WD.Entity.prototype.getLabel = function(langs, returnObject) {
+/**
+ * Get the label for this entity in the first of the languages provided. If none
+ * work, will just return any label. If nothing works at all, returns empty string.
+ * 
+ * @param {String|Array<String>} langs single language or list of languages to try
+ * @return {String}
+ */
+WD.Entity.prototype.getLabel = function(langs) {
   if(langs instanceof String) {
     langs = [ langs ];
   }
 
   // iterate through langs until we find one we have
   for(var i = 0; i < langs.length; ++i) {
-    var obj = this.entity.labels[langs[i]];
-    if(obj) {
-      if (returnObject) {
-        return obj;
-      } else {
-        return obj.value;
-      }
+    if (langs[i] in this.entity.labels) {
+      return this.entity.labels[langs[i]].value;
     }
   }
 
   // no luck. Try to return any label.
   for(var lang in this.entity.labels) {
-    if(returnObject) {
-      return this.entity.labels[lang];
-    } else {
-      return this.entity.labels[lang].value;
-    }
+    return this.entity.labels[lang].value;
   }
 
   // still nothing?!? return empty string
   return "";
 }
+
+/**
+ * Get the Wikidata URL for this entity
+ * @return {String}
+ */
 WD.Entity.prototype.getUrl = function() {
   return 'https://www.wikidata.org/wiki/' + this.entity.title;
 }
